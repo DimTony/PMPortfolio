a87e00850f68843f11ba0fd29eb6b4a81aa4ad04
DimTony <dhymyantt1@gmail.com>
2024-07-23 23:07:56 +0100

    chore/content update 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+
+const cache = createCache({
+  key: 'css',
+  prepend: true,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CacheProvider value={cache}>
+      <App />
+    </CacheProvider>
   </React.StrictMode>
 );
